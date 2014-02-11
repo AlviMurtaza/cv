778548f3ef753757915e9f5f056994f0782a9f4e
AlviMurtaza <smurtazama@gmail.com>
2014-02-11 20:18:10 +0530

    Updates

--- script/script.js
+++ script/script.js
@@ -3,9 +3,9 @@ $(document).ready(function(){
     $('#education, #interests').hide('fast');
 
     $('#expheadtitle').click(function(){
-    // Hide all the DIVs, and show just the one we want        
- 		$(this).find('i').toggleClass('fa-chevron-circle-up');
-	   	$(this).find('i').toggleClass('fa-chevron-circle-down');
+    // Hide all the DIVs, and show just the one we want
+ 		// $(this).find('i').toggleClass('fa-chevron-circle-up');
+	  //  	$(this).find('i').toggleClass('fa-chevron-circle-down');
         $ ('#ulsocial, #education, #interests').hide(100);
         $("#experience").toggle(100);
     });
@@ -13,18 +13,24 @@ $(document).ready(function(){
 
     $('#eduheadtitle').click(function(){
     // Hide all the DIVs, and show just the one we want
-    	$(this).find('i').toggleClass('fa-chevron-circle-down');
-	  	$(this).find('i').toggleClass('fa-chevron-circle-up');
+    // 	$(this).find('i').toggleClass('fa-chevron-circle-down');
+	  	// $(this).find('i').toggleClass('fa-chevron-circle-up');
         $ ('#experience, #ulsocial, #interests').hide(100);
         $("#education").toggle(100);
     });
 
     $('#intheadtitle').click(function(){
-    // Hide all the DIVs, and show just the one we want  
-	  $(this).find('i').toggleClass('fa-chevron-circle-down');
-	  $(this).find('i').toggleClass('fa-chevron-circle-up');
+    // Hide all the DIVs, and show just the one we want
+	  // $(this).find('i').toggleClass('fa-chevron-circle-down');
+	  // $(this).find('i').toggleClass('fa-chevron-circle-up');
         $ ('#experience, #education, #ulsocial').hide(100);
         $("#interests").toggle(100);
+    });
+
+    $('#expheadtitle, #eduheadtitle, #intheadtitle').click(function(){
+    // Change Arrows
+      $(this).find('i').toggleClass('fa-chevron-circle-down');
+      $(this).find('i').toggleClass('fa-chevron-circle-up');
     });
 
 });
