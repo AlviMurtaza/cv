47386001d3379d1653541549d65dfd58deb4cce5
AlviMurtaza <smurtazama@gmail.com>
2014-04-22 13:30:18 +0530

    update experience

--- script/script.js
+++ script/script.js
@@ -1,36 +1,46 @@
 $(document).ready(function(){
 
-    $('#education, #interests').hide('fast');
+//    $('#education, #interests').hide('fast');
 
-    $('#expheadtitle').click(function(){
+//    $('#expheadtitle').click(function(){
     // Hide all the DIVs, and show just the one we want
  		// $(this).find('i').toggleClass('fa-chevron-circle-up');
 	  //  	$(this).find('i').toggleClass('fa-chevron-circle-down');
-        $ ('#ulsocial, #education, #interests').hide(10);
-        $("#experience").toggle(10);
-    });
+//        $ ('#ulsocial, #education, #interests').hide(10);
+//        $("#experience").toggle(10);
+//    });
 
 
-    $('#eduheadtitle').click(function(){
+//    $('#eduheadtitle').click(function(){
     // Hide all the DIVs, and show just the one we want
     // 	$(this).find('i').toggleClass('fa-chevron-circle-down');
 	  	// $(this).find('i').toggleClass('fa-chevron-circle-up');
-        $ ('#experience, #ulsocial, #interests').hide(10);
-        $("#education").toggle(10);
-    });
+//        $ ('#experience, #ulsocial, #interests').hide(10);
+//        $("#education").toggle(10);
+//    });
 
-    $('#intheadtitle').click(function(){
+//    $('#intheadtitle').click(function(){
     // Hide all the DIVs, and show just the one we want
 	  // $(this).find('i').toggleClass('fa-chevron-circle-down');
 	  // $(this).find('i').toggleClass('fa-chevron-circle-up');
-        $ ('#experience, #education, #ulsocial').hide(10);
-        $("#interests").toggle(10);
-    });
+//        $ ('#experience, #education, #ulsocial').hide(10);
+//        $("#interests").toggle(10);
+//    });
 
-    $('#expheadtitle, #eduheadtitle, #intheadtitle').click(function(){
+//    $('#expheadtitle, #eduheadtitle, #intheadtitle').click(function(){
     // Change Arrows
-      $(this).find('i').toggleClass('fa-chevron-circle-down');
-      $(this).find('i').toggleClass('fa-chevron-circle-up');
-    });
+//      $(this).find('i').toggleClass('fa-chevron-circle-down');
+//      $(this).find('i').toggleClass('fa-chevron-circle-up');
+//    });
+
+//});
+$('.expbody').hide();
+$('.exp h3').on('click', function(e) {
+ // $(this).next('.expbody').slideToggle();
+    e.preventDefault();
+    var elem = $(this).next('.expbody')
+        $('.expbody').not(elem).hide('fast');
+        elem.slideToggle('fast');
+});
 
 });
